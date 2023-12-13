fbf23a60c8471052c9ad3fca81090e3ec59c4a18
JillevdW <Jillevdweerd@iCloud.com>
2023-12-13 21:19:33 +0100

    Removes boilerplate plugin configuration

--- src/main/java/dev/jille/barrowsnothingsound/BarrowsNothingSoundConfig.java
+++ src/main/java/dev/jille/barrowsnothingsound/BarrowsNothingSoundConfig.java
@@ -2,18 +2,8 @@ package dev.jille.barrowsnothingsound;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
-import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("barrowsnothingsound")
 public interface BarrowsNothingSoundConfig extends Config
 {
-	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "The message to show to the user when they login"
-	)
-	default String greeting()
-	{
-		return "Hello";
-	}
 }
